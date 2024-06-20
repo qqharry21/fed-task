5b4a2d86cf6303f659aa776182535015a0dd3476
qqharry21 <qqharry21@gmail.com>
2024-06-21 01:17:46 +0800

    feat: add button component

---
+++ src/components/ui/button/button.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import { cn } from '@/lib/utils';
+
+import './style.css';
+
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  variant?: 'primary' | 'outline' | 'link';
+  icon?: React.ReactNode;
+}
+
+export const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ className, variant = 'primary', children, ...props }, ref) => {
+    return (
+      <button
+        ref={ref}
+        className={cn('btn', `btn-${variant}`, className)}
+        {...props}
+      >
+        {children}
+      </button>
+    );
+  }
+);
