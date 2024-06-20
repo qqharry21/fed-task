3dac86ecd3acc8404cb40f2d17c46686169758e8
qqharry21 <qqharry21@gmail.com>
2024-06-20 14:20:36 +0800

    feat: init

---
+++ .eslintrc.cjs
@@ -0,0 +1,40 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+    'plugin:react-hooks/recommended',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['simple-import-sort', 'import', 'react-refresh'],
+  rules: {
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    'simple-import-sort/imports': [
+      'warn',
+      {
+        groups: [
+          ['^@?\\w', '^react\\w'],
+          // Your internal path aliases
+          ['^@/public(\\/.*|$)'],
+          ['^@/components(\\/.*|$)'],
+          ['^@/hooks(\\/.*|$)'],
+          ['^@/styles(\\/.*|$)'],
+          ['^@/lib(\\/.*|$)'],
+          ['^@/utils(\\/.*|$)'],
+
+          ['^\\.\\.(?!/?$)', '^\\.\\./?$'],
+          // Other relative imports. Put same-folder imports and `.` last.
+          ['^\\./(?=.*/)(?!/?$)', '^\\.(?!/?$)', '^\\./?$'],
+          // Style imports (CSS, SCSS)
+          ['^.+\\.s?(css)$'],
+        ],
+      },
+    ],
+    'import/first': 'error',
+    'import/newline-after-import': 'error',
+    'import/no-duplicates': 'error',
+  },
+};
