6b0ded97fea1e06e77a0bee653d4b1b5bb1ede12
qqharry21 <qqharry21@gmail.com>
2024-06-23 00:50:51 +0800

    build: update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite';
 // https://vitejs.dev/config/
 
 export default defineConfig({
-  base: '/<REPO>/',
+  base: '/fed-task/',
   plugins: [react()],
   resolve: {
     alias: {
